7a2c7f4e40e4593ad1172fea92b3195420e3a113
Joshua Carroll <carrolljosh1976@gmail.com>
2023-03-20 20:09:30 -0400

    Added image, text, and styles to Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,10 +5,22 @@ import artificialIntelImage from "../assets/pexels-thisisengineering-3861969.jpg
 export default function Post() {
   return (
     <div className='post'>
-        <img src={artificialIntelImage} alt="" />
+        <img className='postImage' src={artificialIntelImage} alt="" />
         <div className="postInfo">
-            <div className="postCategories"></div>
+            <div className="postCategories">
+                <span className="postCategory">Tech</span>
+                <span className="postCategory">Life</span>
+            </div>
+            <span className='postTitle'>
+                Lorem ipsum dolor sit.
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
         </div>
+        <p className='postDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Aspernatur esse pariatur nihil nostrum assumenda sequi 
+           voluptatem porro velit, voluptatibus placeat?
+        </p>
     </div>
   )
 }
