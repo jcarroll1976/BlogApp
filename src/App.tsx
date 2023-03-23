457377e19790df4bcf3269ffc5fb1ac5864215ab
Joshua Carroll <carrolljosh1976@gmail.com>
2023-03-22 21:38:47 -0400

    Added content to Write component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import Home from './components/Home';
 import Single from './components/Single';
 import Topbar from './components/Topbar';
+import Write from './components/Write';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Single />
+      <Write />
     </div>
   );
 }
--- src/components/Write.tsx
+++ src/components/Write.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { GrAdd } from 'react-icons/gr'
+import './Write.css'
 
 export default function Write() {
   return (
-    <div>Write</div>
+    <div className='write'>
+        <form className="writeForm">
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                 <GrAdd />
+                </label>
+                <input type="file" id="fileInput" style={{display:'none'}} />
+                <input type="text" placeholder='Title' className='writeInput' autoFocus={true} />
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder='Tell your story...' className='writeInput writeText'></textarea>
+            </div>
+        </form>
+    </div>
   )
 }
